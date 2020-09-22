190c90249c802e5e06a60fb29089908888566680
Rifki Rizkia <rifkirizkia91@gmail.com>
2020-09-22 07:40:09 +0700

    Jurnal.java

---
+++ Jurnal1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Jurnal1 {
+	public static void main(String[] args) {
+	Scanner dira = new Scanner(System.in);
+	System.out.println("N:");
+	int bilangan = dira.nextInt();
+	int hasil = bilangan * bilangan;
+	 
+	System.out.println(+ hasil);
+	}
+}
